bb9e340fb2e4c9d13ce4a1c091fb4132c37026fa
Ahmed Ibrahim <ahmedibrhiam512@gmail.com>
2024-03-28 04:48:28 +0200

    add more more

--- src/components/main/Slider.tsx
+++ src/components/main/Slider.tsx
@@ -433,6 +433,75 @@ export default function App() {
           </Button>
         </Box>
       </SwiperSlide>
+      <SwiperSlide className="SwiperSlide">
+        <Box
+          sx={{
+            position: "relative",
+            display: "flex",
+            paddingY: "20px",
+            borderRadius: "5px",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            bgcolor: "#fff",
+            transition: "border-color 0.3s",
+            border: "1px solid transparent",
+            "&:hover": {
+              border: "1px solid",
+            },
+          }}
+        >
+          <AddShoppingCartOutlinedIcon
+            className="icon"
+            sx={{
+              position: "absolute",
+              top: 10,
+              right: 3,
+              opacity: 0,
+              transition: "opacity 0.3s",
+            }}
+          />
+          <FavoriteBorderIcon
+            className="icon"
+            sx={{
+              position: "absolute",
+              top: 40,
+              right: 3,
+              opacity: 0,
+              transition: "opacity 0.3s",
+            }}
+          />
+          <img src="public\all products\bag\1.webp" className="imgSlider" />
+          <Typography variant="body1">Selver ahmed</Typography>
+          <Typography variant="body1">Selver ahmed</Typography>
+          <Rating
+            name="simple-controlled"
+            value={value}
+            sx={{
+              fontSize: "20px",
+            }}
+            onChange={(newValue: any) => {
+              setValue(newValue);
+            }}
+          />
+          <Button
+            sx={{
+              border: "1px solid #000",
+              mb: "8px",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: "12px",
+                color: "black",
+                px: "15px",
+              }}
+            >
+              Add To Cart
+            </Typography>
+          </Button>
+        </Box>
+      </SwiperSlide>
     </Swiper>
   );
 }
